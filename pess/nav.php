ae30b85a2f5655b3587485314e74c9e32c005091
Suzawani <suzzaaaaa@gmail.com>
2019-05-13 12:31:22 +0800

    second commit

---
+++ pess/nav.php
@@ -0,0 +1,7 @@
+<h1>Police Emergency Service Systems</h1>
+<nav>
+    <a href="logcall.php">Log Call</a>
+    <a href="Update.php">Update</a>
+    <a href="#">Report</a>
+    <a href="#">History</a>
+  </nav>
